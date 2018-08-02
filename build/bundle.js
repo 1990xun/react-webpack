b3c71315d248ff8542e085f0b010f31b9734e860
1990xun <565498700@qq.com>
2018-08-02 16:37:55 +0800

    my-app3

---
+++ build/bundle.js
@@ -0,0 +1,116 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__(1);
+
+__webpack_require__(2);
+
+__webpack_require__(3);
+
+//let测试
+var a = 1;
+{
+    var _a = 2;
+}
+console.log(a);
+//()=>测试
+var b = [1, 2, 3, 4];
+b.map(function (x) {
+    console.log(x);
+});
+//换行
+var msg = "\u8FD9\u662F\u4E00\u6BB5\u975E\u5E38\u975E\u5E38\u957F\u7684\u6362\u884C\u6587\u5B57\uFF0CES6\u4E4B\u524D\u662F\u7528\u5355\u5F15\u53F7\u548C\uFF0C\n\u73B0\u5728\u53EF\u4EE5\u76F4\u63A5\u7528`,\u5F88\u65B9\u4FBF\n\u662F\u5427\uFF01";
+console.log(msg);
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+// removed by extract-text-webpack-plugin
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=bundle.js.map
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, 'src/main.js'),       //入口
+    },
+    output: {
+        filename: 'bundle.js',               //输出的文件名
+        path: path.resolve(__dirname, 'build') //输出文件所在的目录
+    },
+    devServer: { // 检测代码变化并自动重新编译并自动刷新浏览器
+        contentBase: path.resolve(__dirname, 'build') // 设置静态资源的根目录
+    },
+    devtool:'source-map',
+    module: { // 如何处理项目中不同类型的模块
+        rules: [ // 用于规定在不同模块被创建时如何处理模块的规则数组
+            {
+                test: /(\.jsx|\.js|\.css)$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "env", "react"
+                        ]
+                    }
+                },
+                exclude: path.resolve(__dirname, 'node_modules'),
+                include: path.resolve(__dirname, 'src'),
+            },
+            {
+                test: /\.css$/,
+                use:ExtractTextPlugin.extract({
+                    fallback:"style-loader",
+                    use:"css-loader"
+                })
+            },
+            {
+                test: /\.scss$/,
+                use:ExtractTextPlugin.extract({
+                    fallback:"style-loader",
+                    use:[{
+                        loader:"css-loader"
+                    },{
+                        loader:"sass-loader",
+                        options: {
+                            outputStyle: "compressed" //生成压缩后的文件
+                        }
+                    }]
+                })
+            },
+            {
+                test: /\.less$/,
+                use:ExtractTextPlugin.extract({
+                    fallback:"style-loader",
+                    use:[{
+                        loader:"css-loader"
+                    },{
+                        loader:"less-loader",
+                        options:{
+                            compress:true   //生成压缩后的文件
+                        }
+                    }]
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('css/index.css') //css scss less文件打包生成单个的css文件
+    ]
+}
